7c8fa6a3a5a200fee5979bb3651c159e45d824b0
Danil Tkachenko <Danil.Tkachenko@getmore.cz>
2023-02-20 08:17:14 +0100

    Add project files.

---
+++ 2048Solver/MonteCarlo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _2048Solver.Game;
+
+namespace _2048Solver
+{
+	internal class MonteCarlo
+	{
+		public Game Game { get; set; }
+		public MonteCarlo(Game game)
+		{
+			Game = game;
+		}
+
+		struct Run
+		{
+			Direction direction;
+
+			int finalScore;
+		}
+
+		internal void MonteCarloInit (ulong[,] Board)
+		{
+			ulong[,] FirstBoard = Board;
+
+			Run run = GetBestMove(Board);
+
+			game
+			Board = FirstBoard;
+
+
+		}
+
+		internal Run GetBestMove(ulong[,] Board)
+		{
+
+
+
+
+
+
+		}
+
+
+
+	}
+}
